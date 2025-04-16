bd5936e3c878d0011e38dc84e6eb53f45d14dd54
Anniele Marinho <annimarinho14@gmail.com>
2025-04-16 16:52:01 -0300

    Criação de arquivos

---
+++ inicio/comentario.js
@@ -0,0 +1,10 @@
+//console.log é usado para exibr algo na tela do computador
+console.log('Passo #01');
+console.log('Passo #02');
+//console.log('Passo #03');
+
+/*
+    Esse é um comentário
+    de múltiplas linhas! 
+*/
+console.log('Passo #04');
